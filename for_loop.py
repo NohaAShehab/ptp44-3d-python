9ba506f98480667384ff08d3cb75ac33ce7bb803
Noha Shehab <nohashehab.iti@gmail.com>
2024-01-15 15:44:21 +0200

    for loop

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+#
+# message = 'we love Python'
+#
+# for char in message:
+#     print(f"char = {char}")
+#
+# print("=================")
+
+""" noha ---> 2 """
+no_of_vowels= 0
+for char in 'noha':
+    if char =='o' or char =='e' or char =='i' or char =='a' or char =='u':
+        no_of_vowels+=1
+print(no_of_vowels)

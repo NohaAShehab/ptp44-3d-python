db4bf8b04fee0a89e7cbe4b6b2eee91db53a8219
Noha Shehab <nohashehab.iti@gmail.com>
2024-01-15 13:54:53 +0200

    string functions

---
+++ string_status.py
@@ -0,0 +1,40 @@
+
+
+""" check string status """
+
+# anyval  = input("please enter any value : ")
+#
+# # print(anyval.isdigit())
+#
+# """ check if string consists of alphas from a - > z only """
+#
+# print(anyval.isalpha())
+
+
+""" check if the string consists only from spaces """
+
+# name = input("please enter your name ")
+# print(name.isspace()) # return True if the string consists of only spaces
+# if name:
+#     print("found")
+# else:
+#     print("-=- not found ---")
+
+
+""" check if the string is upper or lower case """
+
+# name = 'NOHAhjgfg'
+# print(name.islower())
+# print(name.isupper())
+
+
+""" convert string to uppercase / lowercase """
+
+mood = 'statisfied'
+print(mood.upper())  # return string in upper case
+
+status = 'IN PROGRESS'
+print(status.lower())  # return string in lower case
+
+
+

262a1b1a0d7bd510e0d9298d52d09653ebd8e845
Noha Shehab <nohashehab.iti@gmail.com>
2024-01-16 12:20:31 +0200

    lists and while loop

---
+++ list_operationss.py
@@ -0,0 +1,47 @@
+#
+# """  get index of elemenet """
+#
+# newlist =['book', 'pen', 15, 9.8, True, 'jacket', 'iti', 'jacket', 'iti', 'jacket']
+#
+# print(newlist.index("iti"))  # get the index of the first occurence
+#
+# """ list concat"""
+#
+# students = ['ahmed', 'mohamed', 'ali', 'iti', 'python']
+# courses = ['maya', 'animation', 'photoshop']
+#
+# track = students + courses  # return new list
+# print(track)
+#
+# """ extend a list """
+# print(students)
+# students.extend(courses)  # extend the existing list
+# print(students)
+#
+
+""" sort the list elements ---> all the lists elements  must be from the same type """
+
+students_names = ['Ingy', "A'alaa", 'Ola', 'Nesma', 'sara', 'Kareem', 'Salma', 'Nada',
+                  'mahmoud', 'Tasnim','Reem', 'Yosr','Mai', 'Mohamed']
+# print(students_names)
+# students_names.sort(reverse=True)
+# print(students_names)
+#
+#
+
+###########################3
+
+""" reverse the list """
+
+newlist =['book', 'pen', 15, 9.8, True, 'jacket', 'iti', 'jacket','iti', 'jacket']
+print(newlist)
+
+newlist.reverse()
+print(newlist)
+
+
+
+##################################3
+
+
+
---
+++ while_loop.py
@@ -0,0 +1,43 @@
+
+
+""" ask the user to enter 5 elements and save them to the list """
+
+# elements = []
+#
+# item = input("please enter an element: ")
+# elements.append((item))
+#
+# item = input("please enter an element: ")
+# elements.append((item))
+#
+# item = input("please enter an element: ")
+# elements.append((item))
+#
+# item = input("please enter an element: ")
+# elements.append((item))
+# item = input("please enter an element: ")
+# elements.append((item))
+
+#####################################
+
+elements = []
+
+counter =1
+
+while counter < 6:
+    item = input(f"please enter an item {counter} : ")
+    elements.append(item)
+    counter += 1
+
+print(elements)
+
+
+
+
+
+
+
+
+
+
+
